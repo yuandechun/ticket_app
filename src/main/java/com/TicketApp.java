40ef51adf2e6e8bc4a6f841332bf0d2ef93a1fad
De Chun Yuan <42953891+yuandechun@users.noreply.github.com>
2020-05-01 10:41:10 +0800

    update formate

--- src/main/java/com/TicketApp.java
+++ src/main/java/com/TicketApp.java
@@ -10,7 +10,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @ServletComponentScan
 public class TicketApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketApp.class, args);
+    }
 }
